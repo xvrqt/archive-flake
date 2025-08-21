2d31d506ceb976b153625db4823d350f9105cf7c
xvrqt <git@xvrqt.com>
2025-08-21 16:17:03 -0700

    Updated the monero config to just open the rpc port and bind directly to the irlqt-net since that's encrypted and locked down. Users can hit it using the MagicDNS resolution archive.machines.irlqt.net:18081 - which is a better solution than running it through the reverse proxy anyway.

--- nixos/monero.nix
+++ nixos/monero.nix
@@ -1,11 +1,14 @@
-{ pkgs, config, ... }:
+{ lib, pkgs, config, inputs, machine, ... }:
 let
   monero-port = 18080;
   monero-rpc-port = 18081;
 
   domain = "irlqt.net";
-  subDomain = "public.monero.nodes.archive";
+  subDomain = "archive.nodes.monero";
 
+  addresses = inputs.networking.config.machines.${machine}.ip.v4;
+  amy-net-interface = addresses.wg;
+  irlqt-net-interface = addresses.tailnet;
 in
 {
 
@@ -14,12 +17,13 @@ in
   ];
 
   networking.firewall = {
-    allowedTCPPorts = [ monero-port monero-rpc-port ];
+    allowedTCPPorts = [ monero-port monero-rpc-port 443 80 ];
   };
 
 
-  # You'll need this when you change to exposing the interface directly
-  # systemd.services.monero.serviceConfig.ExecStart = lib.mkForce "${lib.getExe' pkgs.monero-cli "monerod"} --config-file=${config.services.monero.dataDir}/monerod.conf --non-interactive --confirm-external-bind";
+  # Adding --confirm-external-bind to the startup command (otherwise it won't start)
+  # because we're binding to the irqt-net
+  systemd.services.monero.serviceConfig.ExecStart = lib.mkForce "${lib.getExe' pkgs.monero-cli "monerod"} --config-file=${config.services.monero.dataDir}/monerod.conf --non-interactive --confirm-external-bind";
 
   services = {
     monero = {
@@ -29,12 +33,12 @@ in
 
       rpc = {
         port = monero-rpc-port;
-        address = "127.0.0.1";
+        address = irlqt-net-interface;
         restricted = false;
       };
 
       limits = {
-        # threads = 8;
+        # threads = 1;
         upload = 1024; # 1 MiB total
       };
 
@@ -42,36 +46,6 @@ in
         enable = false;
         threads = 1;
         address = "89BkGnoQ1T8KpzuBS2gpkoGcke9r8smfaMZzAexr1QdXT4tSecEJogXDt428qo5msCKPfHdyZASiF3QnoZmvBDXvAJPUq7o";
-      };
-    };
-
-    # RPC complains if we bind directly to a secure interface and wants us to
-    # pass a flag to confirm a direct bind to an interface. We can't easily do
-    # that without updating the package, and we want to be public anyways so...
-    nginx = {
-      virtualHosts."${subDomain}.${domain}" = {
-        # Only listen on private interfaces
-        listenAddresses = [ "136.27.49.63" "192.168.1.6" "100.64.0.3" "10.128.0.2" ];
-
-        http2 = true;
-        forceSSL = true;
-        acmeRoot = null;
-        enableACME = true;
-
-        locations."/" = {
-          proxyPass = "http://${config.services.monero.rpc.address}:${(toString config.services.monero.rpc.port)}";
-          # proxyWebsockets = true;
-          # recommendedProxySettings = true;
-          extraConfig = ''
-            proxy_set_header Connection "";
-            proxy_http_version 1.1;
-            proxy_read_timeout 600s;
-            # proxy_ssl_server_name on;
-            # proxy_set_header Authorization $http_authorization;
-            # proxy_pass_header  Authorization;
-            # proxy_pass_request_headers on;
-          '';
-        };
       };
     };
   };
